84251c5da5913740500ea46e29b863aa28023038
ColtonTrainingRev <colton367@revature.net>
2023-03-20 12:29:29 -0700

    Week 2 Coding Challenge

--- Java Code/Main.java
+++ Java Code/Main.java
@@ -1,18 +1,30 @@
-
-
+import java.util.ArrayList;
 
 public class Main{
-    
+    static ArrayList<Integer> multiples(int num, int length){
+        ArrayList<Integer> retVal = new ArrayList<Integer>();
+
+        for(int i = 1; i <= length; ++i){
+            retVal.add(num*i);
+        }
+        return retVal;
+    }
+    public static void main(String args[]){
+        System.out.println(multiples(1,10));
+        System.out.println(multiples(5,10));
+        System.out.println(multiples(10,10));
+    }
+}
+    //=========================== WEEK 1 CODE ===================================
     // static int add(int x, int y){ //This can also be used as a sum function
     //     return x+y;
     // }
-    static int convert(int x){ //converts minutes to seconds
-        return x*60;
-    }
+    // static int convert(int x){ //converts minutes to seconds
+    //     return x*60;
+    // }
     
-    public static void main(String args[]){
-        System.out.println(convert(5));
-        System.out.println(convert(3));
-        System.out.println(convert(2));
-    }
-}
+    // public static void main(String args[]){
+    //     System.out.println(convert(5));
+    //     System.out.println(convert(3));
+    //     System.out.println(convert(2));
+    // }
