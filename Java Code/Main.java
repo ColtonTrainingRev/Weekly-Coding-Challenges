c8e1b05b88d868a4cdbbb29554b8ceb9eb9d3ac7
ColtonTrainingRev <colton367@revature.net>
2023-03-23 09:56:52 -0700

    Week 2 Coding Challenge 2

--- Java Code/Main.java
+++ Java Code/Main.java
@@ -1,18 +1,13 @@
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class Main{
-    static ArrayList<Integer> multiples(int num, int length){
-        ArrayList<Integer> retVal = new ArrayList<Integer>();
-
-        for(int i = 1; i <= length; ++i){
-            retVal.add(num*i);
-        }
-        return retVal;
+    static boolean isEqual(int x, int y){
+        return x == y ? true : false;
     }
     public static void main(String args[]){
-        System.out.println(multiples(1,10));
-        System.out.println(multiples(5,10));
-        System.out.println(multiples(10,10));
+        System.out.println(isEqual(4, 8));
+        System.out.println(isEqual(2, 2));
+        System.out.println(isEqual(42, 32));
     }
 }
     //=========================== WEEK 1 CODE ===================================
@@ -27,4 +22,18 @@ public class Main{
     //     System.out.println(convert(5));
     //     System.out.println(convert(3));
     //     System.out.println(convert(2));
+    // }
+    //============================================ WEEK 2 CODE ==================================================
+    // static ArrayList<Integer> multiples(int num, int length){
+    //     ArrayList<Integer> retVal = new ArrayList<Integer>();
+
+    //     for(int i = 1; i <= length; ++i){
+    //         retVal.add(num*i);
+    //     }
+    //     return retVal;
+    // }
+    // public static void main(String args[]){
+    //     System.out.println(multiples(1,10));
+    //     System.out.println(multiples(5,10));
+    //     System.out.println(multiples(10,10));
     // }
